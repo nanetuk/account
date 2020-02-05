4e2faa644b602c11f048c887b9fad84928e70b90
bartek <45334415+nanetuk@users.noreply.github.com>
2020-02-05 12:19:11 +0000

    duplicate goals to schedule

---
+++ modules/schedule/language/english/schedule_lang.php
@@ -0,0 +1,54 @@
+<?php
+
+# Version 2.3.0
+
+$lang['schedules']                                                          = 'Schedules';
+$lang['schedule']                                                           = 'Schedule';
+$lang['schedules_tracking']                                                 = 'Schedules Tracking';
+$lang['new_schedule']                                                       = 'New schedule';
+$lang['schedule_lowercase']                                                 = 'schedule';
+$lang['schedule_start_date']                                                = 'Start Date';
+$lang['schedule_end_date']                                                  = 'End Date';
+$lang['schedule_subject']                                                   = 'Subject';
+$lang['schedule_description']                                               = 'Description';
+$lang['schedule_type']                                                      = 'schedule Type';
+$lang['schedule_achievement']                                               = 'Achievement';
+$lang['schedule_contract_type']                                             = 'Contract Type';
+$lang['schedule_notify_when_fail']                                          = 'Notify staff members when schedule failed to achieve';
+$lang['schedule_notify_when_achieve']                                       = 'Notify staff members when schedule achieve';
+$lang['schedule_progress']                                                  = 'Progress';
+$lang['schedule_total']                                                     = 'Total: %s';
+$lang['schedule_result_heading']                                            = 'schedule Progress';
+$lang['schedule_income_shown_in_base_currency']                             = 'Total income is shown in your base currency';
+$lang['schedule_notify_when_end_date_arrives']                              = 'The staff members will be notified when the end date arrives (Requires CRON).';
+$lang['schedule_staff_members_notified_about_achievement']                  = 'The staff members are notified about this schedule achievement';
+$lang['schedule_staff_members_notified_about_failure']                      = 'Staff member are notified about the failure';
+$lang['schedule_notify_staff_manually']                                     = 'Notify Staff Members Manually';
+$lang['schedule_notify_staff_notified_manually_success']                    = 'The staff members are notified about this schedule result';
+$lang['schedule_notify_staff_notified_manually_fail']                       = 'Failed to notify staff members about this schedule result';
+$lang['schedule_achieved']                                                  = 'Achieved';
+$lang['schedule_failed']                                                    = 'Failed';
+$lang['schedule_close']                                                     = 'Very Close';
+$lang['schedule_type_total_income']                                         = 'Achieve Total Income';
+$lang['schedule_type_convert_leads']                                        = 'Convert X Leads';
+$lang['schedule_type_increase_customers_without_leads_conversions']         = 'Increase Customer Number';
+$lang['schedule_type_increase_customers_without_leads_conversions_subtext'] = 'Leads Conversion is Excluded';
+$lang['schedule_type_increase_customers_with_leads_conversions']            = 'Increase Customer Number';
+$lang['schedule_type_increase_customers_with_leads_conversions_subtext']    = 'Leads Conversions is Included';
+$lang['schedule_type_make_contracts_by_type_calc_database']                 = 'Make Contracts By Type';
+$lang['schedule_type_make_contracts_by_type_calc_database_subtext']         = 'Is calculated from the date added to database';
+$lang['schedule_type_make_contracts_by_type_calc_date']                     = 'Make Contracts By Type';
+$lang['schedule_type_make_contracts_by_type_calc_date_subtext']             = 'Is calculated from the contract start date';
+$lang['schedule_type_total_estimates_converted']                            = 'X Estimates Conversion ';
+$lang['schedule_type_total_estimates_converted_subtext']                    = 'Will be taken only estimates that will be converted to invoices';
+$lang['schedule_type_income_subtext']                                       = 'Income will be calculated in your base currency (not converted)';
+$lang['not_schedule_message_success']                                       = 'Congratulations! We achieved new schedule.<br /> schedule Type: %s
+<br />schedule Achievement: %s
+<br />Total Achievement: %s
+<br />Start Date: %s
+<br />End Date: %s';
+$lang['not_schedule_message_failed'] = 'We failed to achieve schedule!<br /> schedule Type: %s
+<br />schedule Achievement: %s
+<br />Total Achievement: %s
+<br />Start Date: %s
+<br />End Date: %s';
--- modules/schedule/schedule.php
+++ modules/schedule/schedule.php
@@ -1,13 +1,220 @@
 <?php
 
-/**
- * Ensures that the module init file can't be accessed directly, only within the application.
- */
 defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 Module Name: Schedule
-Description: Work schedule for employees.
-Version: 1.1.0
+Description: Work schedule for employees
+Version: 2.3.0
 Requires at least: 2.3.*
 */
+
+define('SCHEWDULE_MODULE_NAME', 'schedule');
+
+hooks()->add_action('after_cron_run', 'schedule_notification');
+hooks()->add_action('admin_init', 'schedule_module_init_menu_items');
+hooks()->add_action('staff_member_deleted', 'schedule_staff_member_deleted');
+hooks()->add_action('admin_init', 'schedule_permissions');
+
+hooks()->add_filter('migration_tables_to_replace_old_links', 'schedule_migration_tables_to_replace_old_links');
+hooks()->add_filter('global_search_result_query', 'schedule_global_search_result_query', 10, 3);
+hooks()->add_filter('global_search_result_output', 'schedule_global_search_result_output', 10, 2);
+hooks()->add_filter('get_dashboard_widgets', 'schedule_add_dashboard_widget');
+
+function schedule_add_dashboard_widget($widgets)
+{
+    $widgets[] = [
+            'path'      => 'schedule/widget',
+            'container' => 'right-4',
+        ];
+
+    return $widgets;
+}
+
+function schedule_staff_member_deleted($data)
+{
+    $CI = &get_instance();
+    $CI->db->where('staff_id', $data['id']);
+    $CI->db->update(db_prefix() . 'schedule', [
+            'staff_id' => $data['transfer_data_to'],
+        ]);
+}
+
+function schedule_global_search_result_output($output, $data)
+{
+    if ($data['type'] == 'schedule') {
+        $output = '<a href="' . admin_url('schedule/schedule/' . $data['result']['id']) . '">' . $data['result']['subject'] . '</a>';
+    }
+
+    return $output;
+}
+
+function schedule_global_search_result_query($result, $q, $limit)
+{
+    $CI = &get_instance();
+    if (has_permission('schedule', '', 'view')) {
+        // Schedule
+        $CI->db->select()->from(db_prefix() . 'schedule')->like('description', $q)->or_like('subject', $q)->limit($limit);
+
+        $CI->db->order_by('subject', 'ASC');
+
+        $result[] = [
+                'result'         => $CI->db->get()->result_array(),
+                'type'           => 'schedule',
+                'search_heading' => _l('schedule'),
+            ];
+    }
+
+    return $result;
+}
+
+function schedule_migration_tables_to_replace_old_links($tables)
+{
+    $tables[] = [
+                'table' => db_prefix() . 'schedule',
+                'field' => 'description',
+            ];
+
+    return $tables;
+}
+
+function schedule_permissions()
+{
+    $capabilities = [];
+
+    $capabilities['capabilities'] = [
+            'view'   => _l('permission_view') . '(' . _l('permission_global') . ')',
+            'create' => _l('permission_create'),
+            'edit'   => _l('permission_edit'),
+            'delete' => _l('permission_delete'),
+    ];
+
+    register_staff_capabilities('schedule', $capabilities, _l('schedule'));
+}
+
+function schedule_notification()
+{
+    $CI = &get_instance();
+    $CI->load->model('schedule/schedule_model');
+    $schedule = $CI->schedule_model->get('', true);
+    foreach ($schedule as $schedule) {
+        $achievement = $CI->schedule_model->calculate_schedule_achievement($schedule['id']);
+        if ($achievement['percent'] >= 100) {
+            if ($schedule['notify_when_achieve'] == 1) {
+                if (date('Y-m-d') >= $schedule['end_date']) {
+                    $CI->schedule_model->notify_staff_members($schedule['id'], 'success', $achievement);
+                }
+            }
+        } else {
+            // not yet achieved, check for end date
+            if ($schedule['notify_when_fail'] == 1) {
+                if (date('Y-m-d') > $schedule['end_date']) {
+                    $CI->schedule_model->notify_staff_members($schedule['id'], 'failed', $achievement);
+                }
+            }
+        }
+    }
+}
+
+/**
+* Register activation module hook
+*/
+register_activation_hook(SCHEDULE_MODULE_NAME, 'schedule_module_activation_hook');
+
+function schedule_module_activation_hook()
+{
+    $CI = &get_instance();
+    require_once(__DIR__ . '/install.php');
+}
+
+/**
+* Register language files, must be registered if the module is using languages
+*/
+register_language_files(SCHEDULE_MODULE_NAME, [SCHEDULE_MODULE_NAME]);
+
+/**
+ * Init schedule module menu items in setup in admin_init hook
+ * @return null
+ */
+function schedule_module_init_menu_items()
+{
+    $CI = &get_instance();
+
+    $CI->app->add_quick_actions_link([
+            'name'       => _l('schedule'),
+            'url'        => 'schedule/schedule',
+            'permission' => 'schedule',
+            'position'   => 56,
+            ]);
+
+    if (has_permission('schedule', '', 'view')) {
+        $CI->app_menu->add_sidebar_children_item('utilities', [
+                'slug'     => 'schedule-tracking',
+                'name'     => _l('schedule'),
+                'href'     => admin_url('schedule'),
+                'position' => 24,
+        ]);
+    }
+}
+
+
+/**
+ * Get schedule types for the schedule feature
+ * @return array
+ */
+function get_schedule_types()
+{
+    $types = [
+        [
+            'key'      => 1,
+            'lang_key' => 'schedule_type_total_income',
+            'subtext'  => 'schedule_type_income_subtext',
+        ],
+        [
+            'key'      => 2,
+            'lang_key' => 'schedule_type_convert_leads',
+        ],
+        [
+            'key'      => 3,
+            'lang_key' => 'schedule_type_increase_customers_without_leads_conversions',
+            'subtext'  => 'schedule_type_increase_customers_without_leads_conversions_subtext',
+        ],
+        [
+            'key'      => 4,
+            'lang_key' => 'schedule_type_increase_customers_with_leads_conversions',
+            'subtext'  => 'schedule_type_increase_customers_with_leads_conversions_subtext',
+        ],
+        [
+            'key'      => 5,
+            'lang_key' => 'schedule_type_make_contracts_by_type_calc_database',
+            'subtext'  => 'schedule_type_make_contracts_by_type_calc_database_subtext',
+        ],
+        [
+            'key'      => 7,
+            'lang_key' => 'schedule_type_make_contracts_by_type_calc_date',
+            'subtext'  => 'schedule_type_make_contracts_by_type_calc_date_subtext',
+        ],
+        [
+            'key'      => 6,
+            'lang_key' => 'schedule_type_total_estimates_converted',
+            'subtext'  => 'schedule_type_total_estimates_converted_subtext',
+        ],
+    ];
+
+    return hooks()->apply_filters('get_schedule_types', $types);
+}
+/**
+ * Translate schedule type based on passed key
+ * @param  mixed $key
+ * @return string
+ */
+function format_schedule_type($key)
+{
+    foreach (get_schedule_types() as $type) {
+        if ($type['key'] == $key) {
+            return _l($type['lang_key']);
+        }
+    }
+
+    return $type;
+}
