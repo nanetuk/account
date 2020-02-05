2127af6341764e709423caf09850ae13c9ac9bde
bartek <45334415+nanetuk@users.noreply.github.com>
2020-02-05 11:13:34 +0000

    module description

---
+++ modules/schedule/schedule.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Ensures that the module init file can't be accessed directly, only within the application.
+ */
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/*
+Module Name: Schedule
+Description: Work schedule for employees.
+Version: 1.1.0
+Requires at least: 2.3.*
+*/
